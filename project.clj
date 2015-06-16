5b7f5d0ab47efbe16aaf0587ff8a324a1378916c
Tienson Qin <tiensonqin@gmail.com>
2015-06-16 14:29:57 +0800

    base on environ

--- project.clj
+++ project.clj
@@ -1,7 +1,9 @@
-(defproject environ-plus "0.1.6"
+(defproject environ-plus "0.1.7"
   :description "Core environ-plus library for use in applications"
   :url "https://github.com/tiensonqin/environ-plus"
   :scm {:dir ".."}
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.6.0"]])
+  :dependencies [[environ "1.0.0"]]
+  :aot :all
+  :profiles {:dev {:dependencies [[org.clojure/clojure "1.6.0"]]}})
--- src/environ_plus/core.clj
+++ src/environ_plus/core.clj
@@ -1,27 +1,8 @@
 (ns environ-plus.core
-  (:require [clojure.string :as str]
-            [clojure.java.io :as io]))
-
-(defn- keywordize [s]
-  (-> (str/lower-case s)
-      (str/replace "_" "-")
-      (str/replace "." "-")
-      (keyword)))
-
-(defn- sanitize [k]
-  (let [s (keywordize (name k))]
-    (if-not (= k s) (println "Warning: environ-plus key " k " has been corrected to " s))
-    s))
-
-(defn- read-system-env []
-  (->> (System/getenv)
-       (map (fn [[k v]] [(keywordize k) v]))
-       (into {})))
-
-(defn- read-system-props []
-  (->> (System/getProperties)
-       (map (fn [[k v]] [(keywordize k) v]))
-       (into {})))
+  (:require [environ.core :as environ]
+            [clojure.edn :as edn]
+            [clojure.java.io :as io])
+  (:import java.io.PushbackReader))
 
 (defn read-resource [resource]
   (try (let [env-file (io/resource resource)]
@@ -30,21 +11,16 @@
        (catch Exception e
          nil)))
 
-(defn get-environment
-  [envs]
-  (case (:environment envs)
-        "test" "test"
-        "ci" "ci"
-        "production" "production"
-        "development"))
+(defn- read-config-file [path]
+  (try
+    (with-open [r (-> path io/resource io/reader PushbackReader.)]
+      (edn/read r))
+    (catch Exception e
+      (println (str "WARNING: edn-config: " (.getLocalizedMessage e))))))
 
 (defonce ^{:doc "A map of environment variables."}
   env
-  (let [envs (read-system-env)
-        props (read-system-props)
-        environment (get-environment envs)
-        config (read-resource (str "config/" environment ".clj"))]
+  (let [config (read-config-file (str "config/" (:environment environ/env) ".edn"))]
     (merge
      config
-     (assoc envs :environment (keyword environment))
-     props)))
+     environ/env)))
